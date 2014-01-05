d133187ecd1274f1b3eef314bd9967f9e0a6c7ab
PMitrafanau <pmitrafanau@exadel.com>
2014-01-05 18:29:55 +0300

    Done:)

---
+++ src/main/java/com/exadel/certificates/util/PemUtils.java
@@ -0,0 +1,22 @@
+package com.exadel.certificates.util;
+
+
+import org.bouncycastle.openssl.PEMWriter;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class PemUtils {
+
+    private PemUtils() {
+
+    }
+
+
+    public static void saveAsFile(Object savingObject, String path) throws IOException {
+        PEMWriter pemWriter = new PEMWriter(new PrintWriter(new File(path)));
+        pemWriter.writeObject(savingObject);
+        pemWriter.close();
+    }
+}
